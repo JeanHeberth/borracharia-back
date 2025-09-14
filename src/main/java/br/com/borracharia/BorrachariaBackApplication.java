29d9f9cb42d2af89902deb9fc2cedd2744e51a96
jeanheberth <jeanheberth19@gmail.com>
2025-09-14 17:21:41 -0300

    Initial commit

---
+++ src/main/java/br/com/borracharia/BorrachariaBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.borracharia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrachariaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BorrachariaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/borracharia/BorrachariaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.borracharia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorrachariaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
